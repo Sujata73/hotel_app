016e7ba67081713579d78f4c99d01bab39aca171
Sujata Shrestha <shresthasujata300@gmail.com>
2022-04-23 10:55:37 +0545

    deploy

--- hotel_reservation/settings.py
+++ hotel_reservation/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '$q=*u9=pj17w@x6e*m9%5x26o3=^xpbamrtxo#6dpc3@^)8jyn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://deploy-hotel-reservation.herokuapp.com','localhost','127.0.0.1']
 
 
 # Application definition
@@ -123,3 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
